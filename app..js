3649bf09bb2dcd695694b024c934ccebf02aaa4d
aalka061 <a_algg@hotmail.com>
2018-04-24 16:05:01 -0400

    adding create feature

--- app..js
+++ app..js
@@ -1,11 +1,13 @@
 var express = require('express');
 
 var app = new express();
-
+var bodyParser= require ('body-parser');
 
 app.use('/assets', express.static(__dirname + '/public/assets') );
 
 app.set('view engine', 'ejs')
+
+app.use(bodyParser.json())
 
 var port = process.env.PORT || 1776;
 
@@ -30,9 +32,23 @@ var products = [
 
 ]
 
+var currentId = 2;
+
 app.get('/products', function(req, res){
 
   res.send({products: products});
+})
+
+app.post('/products', function(req, res){
+    
+  var name = req.body.name;
+  currentId++;
+
+  products.push({
+    id: currentId,
+    name: name
+  })
+  res.send('success')
 })
 
 app.listen(port);
--- public/assets/script.js
+++ public/assets/script.js
@@ -1,6 +1,6 @@
 $(function(){
     //GET request
-    $('#createInput').on('click', function(){
+    $('#getProduct').on('click', function(){
         $.ajax({
 
             url: '/products',
@@ -16,10 +16,38 @@ $(function(){
                     tobodyElement.append('\
                         <tr>\
                              <td class="id">' + product.id + '</td>'+ 
-                             '<td class="name">' + product.name + '</td> \
+                             '<td> <input type="text" class="name" value="' + product.name +'">' + '</td>'+ 
+                             '<td>\
+                                  <button class="updateButton"> Update/PUT </button>\
+                                  <button class="deleteButton"> DELETE </button>\
+                             </td>\
                         </tr>');
                 })
             }
         })
     })
+
+    $('#createProductForm').on('submit', function(event){
+
+        event.preventDefault();
+
+        var createInput = $('#user-input');
+        $.ajax ({
+            url: '/products',
+            method: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify({name: createInput.val()}),
+            success: function (res){
+                console.log(res);
+                createInput.val('');
+                $('#getProduct').click();
+                
+            }
+
+        })
+    })
+
+    //Post request 
+
+    
 });
